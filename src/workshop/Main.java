c40b11d23a63f7bb690b668a0df477d955aadf56
GnuMcDoge <ofk14den@12156-STUD-D.HIG-AD.hig.se>
2016-02-23 14:08:00 +0100

    Work on add data

--- src/workshop/Main.java
+++ src/workshop/Main.java
@@ -1,13 +1,39 @@
 package workshop;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import Lab1.ImplDataSource;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Map<LocalDate, Double> temperaturDataMap = new HashMap<LocalDate, Double>();
+		Map<LocalDate, Double> goalDataMap = new HashMap<LocalDate, Double>();
 		TemperatureSource test = new TemperatureSource();
+		FootballGoalsSource goal = new FootballGoalsSource();
+		ImplDataSource dataX = new ImplDataSource("Temerature", "C");
+		ImplDataSource dataY = new ImplDataSource("Goal", "z+");
 		
-		test.getValues();
+		temperaturDataMap = test.getValues();
+		goalData
+		
+		for(LocalDate temperaturKey : temperaturDataMap.keySet())
+		{ 
+			dataX.addData(temperaturKey, dataX.getData().get(temperaturKey));
+		
+		}
+		for(LocalDate goalKey : temperaturDataMap.keySet())
+		{ 
+			dataX.addData(goalKey, dataX.getData().get(goalKey));
+		
+		}
+		
+		
+	
 		
 	}
 
