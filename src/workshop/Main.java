b35b6f855a9d9b6bc6f2dd3ba1c5e1919216e8e5
GnuMcDoge <ofk14den@12156-STUD-D.HIG-AD.hig.se>
2016-02-23 14:13:57 +0100

    Gnu

--- src/workshop/Main.java
+++ src/workshop/Main.java
@@ -5,10 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import Lab1.DataCollectionBuilder;
 import Lab1.ImplDataSource;
+import Lab1.Resolution;
 
 public class Main {
-
+	private static DataCollectionBuilder dcb;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Map<LocalDate, Double> temperaturDataMap = new HashMap<LocalDate, Double>();
@@ -17,21 +19,25 @@ public class Main {
 		FootballGoalsSource goal = new FootballGoalsSource();
 		ImplDataSource dataX = new ImplDataSource("Temerature", "C");
 		ImplDataSource dataY = new ImplDataSource("Goal", "z+");
+		dcb = new DataCollectionBuilder(dataX, dataY, Resolution.DAY);
+		
 		
 		temperaturDataMap = test.getValues();
-		goalData
+		goalDataMap = goal.getValues();
 		
 		for(LocalDate temperaturKey : temperaturDataMap.keySet())
 		{ 
 			dataX.addData(temperaturKey, dataX.getData().get(temperaturKey));
 		
 		}
-		for(LocalDate goalKey : temperaturDataMap.keySet())
+		for(LocalDate goalKey : goalDataMap.keySet())
 		{ 
-			dataX.addData(goalKey, dataX.getData().get(goalKey));
+			dataY.addData(goalKey, dataY.getData().get(goalKey));
 		
 		}
 		
+		
+		System.out.println(dcb.getResult());
 		
 	
 		
