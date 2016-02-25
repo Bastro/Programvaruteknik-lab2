3861c901409e8258036b67326a5fcb0bdbbe9e27
Jonas Öster <jonas_leker@hotmail.com>
2016-02-25 15:53:55 +0100

    lag till lite comments

--- src/workshop/TemperatureSource.java
+++ src/workshop/TemperatureSource.java
@@ -5,31 +5,50 @@ import java.util.Map;
 
 import Lab1.DataSource;
 
+/**
+ * @author ofk14den
+ * @author Jonas Oster
+ */
 public class TemperatureSource implements DataSource {
+	private String name;
+	private String unit;
+	
+	/**
+	 * Name value is where the temprature is taken
+	 * @param name
+	 * @param unit
+	 */
+	public TemperatureSource(String name, String unit) {
+		this.name = name;
+		this.unit = unit;
+	}
+
+	/**
+	 * Sets default values
+	 */
+	public TemperatureSource() {
+		name = "Gävle";
+		unit = "C";
+	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return "Temperatur for Gävle";
+		return "Temperatur for " + name;
 	}
 
 	@Override
 	public String getUnit() {
-		// TODO Auto-generated method stub
-		return "C";
+		return unit;
 	}
 
 	@Override
 	public Map<LocalDate, Double> getData() {
-		 UrlFetcher csvFetcher = new UrlFetcher("http://opendata-download-metobs.smhi.se/explore/zip?parameterIds=2&stationId=107420&period=corrected-archive&includeMetadata=false");
-		 CsvToMapParser parsed = new CsvToMapParser(csvFetcher.getContent());
-		Map<LocalDate,Double> data = parsed.getResult( );
+		UrlFetcher csvFetcher = new UrlFetcher(
+				"http://opendata-download-metobs.smhi.se/explore/zip?parameterIds=2&stationId=107420&period=corrected-archive&includeMetadata=false");
+		CsvToMapParser parsed = new CsvToMapParser(csvFetcher.getContent());
+		Map<LocalDate, Double> data = parsed.getResult();
 
-		
 		return data;
 	}
-	
-	
-	
 
 }
